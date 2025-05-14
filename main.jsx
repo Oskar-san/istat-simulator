7efede735409a3af7e06211168f3b8b6dd5e8996
Oskar-san <oskar_san@hotmail.com>
2025-05-14 12:15:29 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IStatSimulator from './components/IStatSimulator'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IStatSimulator />
+  </React.StrictMode>,
+)
