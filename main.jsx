10e227ffff97b52eac9bc6afd341044174917220
Oskar-san <oskar_san@hotmail.com>
2025-05-14 12:26:26 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import IStatSimulator from './components/IStatSimulator'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <IStatSimulator />
-  </React.StrictMode>,
-)
